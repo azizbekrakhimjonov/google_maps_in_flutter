8705de74f2d699f6d9bc623363834a85aafce912
azizbekrahimjonov <azizbekrahimjonov571@gmail.com>
2024-03-16 17:10:36 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green[700],
+      ),
+      home: Scaffold(
+        body: Map(),
+      ),
+    );
+  }
+}
+
+class Map extends StatelessWidget {
+  final Completer<GoogleMapController> _controller = Completer();
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  Map({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
+            ),
+          ),
+          // Text('test')
+        ],
+      ),
+    );
+  }
+}
